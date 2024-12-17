bad7da7ded333ba231b2b9e309263139ba61c072
Saradmgr <saradkunwor20@gmail.com>
2024-12-17 14:05:06 +0545

    To Do List

---
+++ todolist/ToDOList.js
@@ -0,0 +1,38 @@
+const inputBox = document.getElementById("text-entered");
+const listContainer = document.getElementById("list-container");
+function addText() {
+  if (inputBox.value === "") {
+    alert("please Enter Something");
+  } else {
+    let li = document.createElement("li");
+    li.innerHTML = inputBox.value;
+    listContainer.appendChild(li);
+    let span = document.createElement("span");
+    span.innerHTML = "❌";
+    li.appendChild(span);
+  }
+  inputBox.value = "";
+  saveData();
+}
+listContainer.addEventListener(
+  "click",
+  function (e) {
+    if (e.target.tagName === "LI") {
+      e.target.classList.toggle("checked");
+      saveData();
+    } else if (e.target.tagName === "SPAN") {
+      e.target.parentElement.remove();
+      saveData();
+    }
+  },
+  false
+);
+
+function saveData() {
+  localStorage.setItem("data", listContainer.innerHTML);
+}
+function getData() {
+  listContainer.innerHTML = localStorage.getItem("data");
+}
+
+getData();
